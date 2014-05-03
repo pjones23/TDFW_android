632ee83598436b8d05246064faf38c1e28d39ca7
Perron Jones <eaglescb20@yahoo.com>
2014-05-03 15:27:31 -0400

    Fixed a minor playback error

--- src/app/tdfw/TDFWActivity.java
+++ src/app/tdfw/TDFWActivity.java
@@ -8,6 +8,7 @@ import android.media.MediaPlayer.OnErrorListener;
 import android.media.MediaPlayer.OnPreparedListener;
 import android.os.Bundle;
 import android.os.PowerManager;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
@@ -24,14 +25,14 @@ public class TDFWActivity extends Activity {
 		setContentView(R.layout.activity_tdfw);
 		System.out.println(getApplicationContext().toString());
 		playImageBtn = (ImageButton) findViewById(R.id.playBtn);
-		
+
 		// Set the button
 		playImageBtn.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				// TODO Handle sound file playback
-				System.out.println("Testing on click");
+				// Play or stop the sound when the button is clicked.
+				Log.i("Play Button", "Play button clicked.");
 				play();
 			}
 		});
@@ -46,52 +47,70 @@ public class TDFWActivity extends Activity {
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
-		//finish();
+		if (mPlayer != null) {
+			// release the media player when the sound is completed
+			Log.i("TDFW sound", "Releasing the media player");
+			mPlayer.release();
+		}
+		// finish the TDFW activity
+		finish();
 	}
-	
+
 	private void play() {
 		// Create the media playing with the desired sound file
-		mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.td4w);
 		
+		if(mPlayer != null){
+			if(mPlayer.isPlaying()){
+				mPlayer.stop();
+				/*
+				 *  Only add the return statement if we want the button to act as a stop button
+				 *  when the song is playing.
+				 *  Currently, it will only be a play button.
+				 *  The next version may use it.
+				 */
+				//return;
+			}
+			mPlayer.reset();
+		}
+		
+		mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.td4w);
+
 		mPlayer.setOnPreparedListener(new OnPreparedListener() {
-			
+
 			@Override
 			public void onPrepared(MediaPlayer mp) {
-				// TODO Auto-generated method stub
+				// Start playing the sound when the media player is prepared.
 				System.out.println("Media player is prepared");
 				mPlayer.start();
-				
+
 			}
 		});
-		
+
 		mPlayer.setOnCompletionListener(new OnCompletionListener() {
-			
+
 			@Override
 			public void onCompletion(MediaPlayer mp) {
-				// TODO Auto-generated method stub
-				System.out.println("Playback completed");
-				if (mPlayer != null){
-					System.out.println("Releasing media player");
-					mPlayer.release();
-				}
+				Log.i("TDFW sound", "Playback completed.");
 			}
 		});
-		
+
 		mPlayer.setOnErrorListener(new OnErrorListener() {
-			
+
 			@Override
 			public boolean onError(MediaPlayer mp, int what, int extra) {
-				// TODO Auto-generated method stub
+				// Must reset the music player when an error occurs.
+				mPlayer.reset();
 				return false;
 			}
 		});
-		
-		// Setting the wake lock to keep the screen on while the music is playing
-		mPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
-		
+
+		// Setting the wake lock to keep the screen on while the music is
+		// playing
+		mPlayer.setWakeMode(getApplicationContext(),
+				PowerManager.PARTIAL_WAKE_LOCK);
+
 		mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-		
+
 	}
-	
 
 }
